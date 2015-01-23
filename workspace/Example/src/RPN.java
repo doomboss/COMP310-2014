a3055e5af20f6fdc10527393f90f3cd53fe6f70a
Onyi Cho <choo@wit.edu>
2015-01-23 14:02:00 -0500

    Add /Example/src/RPN.java

--- workspace/Example/src/RPN.java
+++ workspace/Example/src/RPN.java
@@ -3,25 +3,99 @@ import java.util.Scanner;
 import java.util.Stack;
 
 
+
+
+
 public class RPN {
-	Stack<Integer> stack = new Stack<Integer>();
 	
-	Stack<Integer> evaluate(String expression) {
+	public static void main(String[] args){
+		RPN rpn = new RPN();
+		System.out.println("Enter the full Expression: ");
+		Scanner keyboard = new Scanner(System.in);
+		String expression = keyboard.nextLine();
+		try{
+			System.out.println("End result: "+rpn.evaluate(expression).peek());
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	public RPN(){
+		super();
+	}
+	
+	protected Stack<Integer> stack = new Stack<Integer>();
+	
+	public Stack<Integer> evaluate(String expression) {
+		int intHolder = 0;
 		StringReader sr = new StringReader(expression);
 		Scanner scanner = new Scanner(sr);
 		while (scanner.hasNext()) {
 			String token = scanner.next();
 			// Check for +, -, /, *
 			if (token.equals("+")) {
+					intHolder = stack.peek();
+					stack.pop();
+					intHolder += stack.peek();
+					stack.pop();
+					stack.push(intHolder);					
+				
 				// do the appropriate thing
 				// pop 2 items off stack, and place result
 				// onto stack
+				
 			}
+			else if (token.equals("-")) {
+				intHolder = stack.peek();
+				stack.pop();
+				intHolder = stack.peek() - intHolder;
+				stack.pop();
+				stack.push(intHolder);
+				
+			}
+			else if (token.equals("*")) {
+				intHolder = stack.peek();
+				stack.pop();
+				intHolder *= stack.peek();
+				stack.pop();
+				stack.push(intHolder);
+
+			}
+			else if (token.equals("/")) {
+				intHolder = stack.peek();
+				stack.pop();
+				if(stack.peek() != 0 && intHolder !=0){
+					intHolder = stack.peek() / intHolder;
+					stack.pop();
+					stack.push(intHolder);
+				}
+				else{
+					stack.pop();
+					stack.push(0);
+				}
+
+			}
+			else{
+				try{
+					stack.push(Integer.parseInt(token));
+				}
+				catch(NumberFormatException nfe){
+					System.out.println("Not a numerical input...");
+					System.exit(1);
+				}
+			}
+			System.out.println("Current top value on stack: "+stack.peek());
 			// catch all case
 			// put the integer on the stack
-			Integer.parseInt(token);
 		}
-		
+		scanner.close();
 		return stack;
+	}
+	
+	
+	
+	void clear(){
+		stack.clear();
 	}
 }
--- workspace/Example/src/RPNTest.java
+++ workspace/Example/src/RPNTest.java
@@ -13,6 +13,17 @@ public class RPNTest {
 		Stack<Integer> stack;
 		stack = calculator.evaluate("25 7 32 + *");
 		assertEquals(975,(int)stack.peek());
+		stack = calculator.evaluate("2 3 + 5 /");
+		assertEquals(1,(int)stack.peek());
+		stack = calculator.evaluate("3 3 - 5 /");
+		assertEquals(0,(int)stack.peek());
+		stack = calculator.evaluate("5 3 - 4 8 * +");
+		assertEquals(34,(int)stack.peek());
+		stack = calculator.evaluate("1 2 + 4 * 5 + 3 -");
+		assertEquals(14,(int)stack.peek());
+		stack = calculator.evaluate("5 1 2 + 4 * + 3 -");
+		assertEquals(14,(int)stack.peek());
+		
 	}
 
 }
