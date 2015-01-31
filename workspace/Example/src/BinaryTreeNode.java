9307a2616c096de3888ccd88507c4b1d66542ecd
Onyi Cho <choo@wit.edu>
2015-01-30 22:17:26 -0500

    added stuff from class to BinaryNodeTree

---
+++ workspace/Example/src/BinaryTreeNode.java
@@ -0,0 +1,37 @@
+
+public class BinaryTreeNode
+	<DataType extends Comparable<DataType>>{
+		DataType data;
+		BinaryTreeNode<DataType>left,right;
+		BinaryTreeNode(DataType data){
+			this.data = data;
+		}
+		void add(DataType moreData){
+			// figure out which side to place it
+			if(data.compareTo(moreData) <= 0){
+				// goes to right hand side
+				if(right == null){
+					//create node
+					right = new BinaryTreeNode<DataType>(moreData);
+				}
+				else{
+					right.add(moreData);
+
+				}
+			}
+			else{
+				if (left == null) {
+					// create a node and call it a day
+					left = new BinaryTreeNode<DataType>(moreData);
+				} else {
+					// keep looking recursively
+					left.add(moreData);
+				}
+			}
+			// put it there
+			
+		}
+		public static void main(String[] args){
+			System.out.println("Hello".compareTo("World"));
+		}
+	}
